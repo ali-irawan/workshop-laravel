24d0f4297c81a336069567ac443dfe403b33a99e
Ali Irawan <boylevantz@gmail.com>
2015-01-29 22:20:15 +0700

    adding menu

---
+++ app/views/includes/menu.blade.php
@@ -0,0 +1,4 @@
+<ul class="nav nav-tabs">
+  <li role="presentation" id="lnkcontact"><a href="/">Contact</a></li>
+  <li role="presentation" id="lnkgallery"><a href="/gallery">Gallery</a></li>
+</ul>
